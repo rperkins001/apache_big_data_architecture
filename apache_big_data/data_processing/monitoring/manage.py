e4d2982e5cd890150450f800393cb5ea8c9141e4
Ryan Perkins <84806371+rperkins001@users.noreply.github.com>
2023-04-25 18:17:10 -0500

    Delete apache_big_data directory

--- apache_big_data/data_processing/monitoring/manage.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-job_scripts = ["job1.py", "job2.py", "job3.py"]
-
-for script in job_scripts:
-    print(f"Running {script}...")
-    subprocess.run(["spark-submit", script])
-    print(f"Finished {script}")
