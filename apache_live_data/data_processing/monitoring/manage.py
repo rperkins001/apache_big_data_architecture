308ac9cd814e2738a0008130c4235a1705703c4a
Ryan Perkins <84806371+rperkins001@users.noreply.github.com>
2023-04-25 18:17:21 -0500

    Delete apache_live_data directory

--- apache_live_data/data_processing/monitoring/manage.py
+++
@@ -1,8 +0,0 @@
-# This is a placeholder for a management script that might
-# include functionality like starting/stopping specific jobs,
-# checking the status of jobs, etc.
-def main():
-    print("Management script functionality placeholder")
-
-if __name__ == "__main__":
-    main()
--- apache_live_data/data_processing/monitoring/monitor.py
+++
@@ -1,8 +0,0 @@
-# This is a placeholder for a monitoring script that might
-# include functionality like monitoring the status of all
-# services, jobs, and resource usage.
-def main():
-    print("Monitoring script functionality placeholder")
-
-if __name__ == "__main__":
-    main()
